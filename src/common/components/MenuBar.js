a37e7b24c874633647d51b81b48520326a75a5a3
Tory <88531407+1two13@users.noreply.github.com>
2022-08-28 10:59:15 +0900

    MenuBar 코드 삭제

--- src/common/components/MenuBar.js
+++ src/common/components/MenuBar.js
@@ -2,10 +2,8 @@ import styled from "styled-components";
 
 const MenuBarBox = styled.div`
   display: flex;
-  /* justify-content: space-between; */
   justify-content: space-between;
   padding: 20px 380px 15px 380px;
-  /* margin: 20px 380px 15px 380px; */
   border-bottom: 1px solid #ebeef2;
   a {
     font-weight: 700;
